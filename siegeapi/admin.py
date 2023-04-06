108f831f0eed492054705e1da11372817e34d46a
Zach <zach@Zachs-MacBook-Pro.local>
2023-04-06 05:50:55 -0600

    Added release field for proper filtering, removed combined fields as they are not needed

--- siegeapi/admin.py
+++ siegeapi/admin.py
@@ -3,8 +3,8 @@ from .models import Siege
 
 
 class SiegeAdmin(admin.ModelAdmin):
-    list_display = ('operator', 'faction', 'gadget', 'equipment', 'equip1', 'equip2', 'armor', 'speed',
-                    'side', 'weapons', 'prim1', 'prim2', 'prim3', 'secon1', 'secon2', 'counters', 'count1', 'count1p', 'count2', 'count2p')
+    list_display = ('operator', 'faction', 'gadget', 'equip1', 'equip2', 'armor', 'speed',
+                    'side', 'prim1', 'prim2', 'prim3', 'secon1', 'secon2', 'count1', 'count1p', 'count2', 'count2p', 'release')
 
 
 # Register your models here.
--- siegeapi/serializers.py
+++ siegeapi/serializers.py
@@ -5,5 +5,5 @@ from .models import Siege
 class SiegeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Siege
-        fields = ('id', 'operator', 'faction', 'gadget', 'equipment', 'equip1', 'equip2', 'armor', 'speed',
-                  'side', 'weapons', 'prim1', 'prim2', 'prim3', 'secon1', 'secon2', 'counters', 'count1', 'count1p', 'count2', 'count2p')
+        fields = ('id', 'operator', 'faction', 'gadget', 'equip1', 'equip2', 'armor', 'speed',
+                  'side', 'prim1', 'prim2', 'prim3', 'secon1', 'secon2', 'count1', 'count1p', 'count2', 'count2p', 'release')
